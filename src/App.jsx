f1562dc7f9eeb92702ac60db1744e95dcf6c4274
Gautam1247 <code.grs12@gmail.com>
2026-02-19 20:54:39 +0530

    i have created the ui of landing page with login button and appropriate designing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import Landing from "./pages/landing.jsx";
+import Landing1 from "./pages/Landing1.jsx";
 import Dashboard from "./pages/dashboard.jsx";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Landing />} />
+      <Route path="/" element={<Landing1 />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
